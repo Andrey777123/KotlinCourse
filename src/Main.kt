33c46ef13aea75747e65a6b3980a76d4fba47c0a
AKalinkin <andrew.kalinkin@mail.ru>
2024-08-20 21:06:37 +0300

    task 2

--- src/Main.kt
+++ src/Main.kt
@@ -2,4 +2,8 @@ fun main(args: Array<String>) {
     var nullExAny: Any? = null
     nullExAny = 11
     var  newValue = nullExAny / 2.5 //newValue имеет тип Double
+
+    var helloString = "hello"
+    var aplanaString = "aplana"
+    println("${helloString + " " + aplanaString}")
 }
